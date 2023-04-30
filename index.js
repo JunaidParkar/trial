ea7d66048c5fc9e9a1714df6e5ca030a91b29b86
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2023-04-30 13:04:17 +0530

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+const port = 4257
+
+app.get("/", (req, res) => {
+    res.send("yoo")
+})
+
+app.get("/about", (req, res) => {
+    res.send("abt")
+})
+
+app.listen(port, () => {
+    console.log(`port ${port}`)
+})
